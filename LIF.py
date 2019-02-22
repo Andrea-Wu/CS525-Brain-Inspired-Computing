d58bcce01cfacf3436318eca639c1a081993ecd7
Andrea Wu <ayw19@rutgers.edu>
2019-02-22 00:31:48 -0500

    lif

--- LIF.py
+++ LIF.py
@@ -7,7 +7,7 @@ def eulers():
     #we need a threshold. 
 
     V_t = 1 #threshold voltage
-    CURRENT = 0.1
+    CURRENT = 2
     R_m = 1 #resistance constant
     C_m = 1 #capacitance constant
     TIME_DIFF = 0.2
@@ -17,14 +17,16 @@ def eulers():
     plotY = []
 
     numSpikes = 0
-    while num_spikes < 10:
+    while numSpikes < 10:
+        print("loooop")
         V_curr = 0
         while True:
-
+            print(V_curr)
             plotY.append(V_curr)
             plotX.append(time)
             dV_dt = (CURRENT - ( V_curr / R_m)) / C_m
-            V_curr = V_curr + dV/dt * TIME_DIFF
+            print(dV_dt)
+            V_curr = V_curr + dV_dt * TIME_DIFF
             time = time+ TIME_DIFF
 
             if V_curr > V_t:
@@ -32,6 +34,7 @@ def eulers():
         numSpikes+=1
 
     plt.plot(plotX, plotY)
+    plt.show()
 
 if __name__ == "__main__":
     eulers()
