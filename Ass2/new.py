f3465a8eab0bba0cd51ee726720cfe72b6d59e96
Andrea Wu <ayw19@rutgers.edu>
2019-03-26 21:13:51 -0400

    omfg

--- Ass2/new.py
+++ Ass2/new.py
@@ -1,32 +1,68 @@
 from random import random
 
-TRAIN_LOOP = 1000
+import math
 
-TIME_PER_NEURON = 10 #time is a social construct. We don't need units!
-LOW_CURR = 1e-4
-HIGH_CURR = 5e-4
+TRAIN_LOOP = 3
 
-ON_TRAIN = 10
-OFF_TRAIN = -1
+TIME_PER_NEURON = 100 #time is a social construct. We don't need units!
+TIME_DIFF = 0.01
+LOW_CURR = 2e-3
+HIGH_CURR = 5e-3
 
-CTR = 1
+
+ON_TRAIN = 1
+OFF_TRAIN = -100
+
+CASE1_RATE = -1
+CASE3_RATE = -1
+
+negativeWeightCtr = 0
+
+CTR = 100
+
+def currToRate(curr):
+    m = 3124.674377412921
+    b = 1.3024903596836612e-05   
+
+    rate = (m * curr) + b
+    return rate
+
+def rateToCurr(rate):
+    m = 3124.674377412921
+    b = 1.3024903596836612e-05   
+    curr = (rate - b) / m
+    return curr
+
 class TrainingNeuron:
     def __init__(self, student):
         self.train = []
         self.curr = 0
 
     def updateCurrent(self, curr):
-        m = 3124.674377412921
-        b = 1.3024903596836612e-05   
+        self.curr = curr
 
         #calculate a "constant" current of spikes
-        rate = (m * curr) + b 
+        rate = currToRate(curr)
         timeDiff = 1/rate
 
         #set the spike train
         self.train = []
         self.train.append(0)
         self.train.append(timeDiff)
+
+    def updateCurrWithRate(self, rate):
+        #calculate current
+        self.curr = rateToCurr(rate)
+        
+        #calculate a "constant" current of spikes
+        timeDiff = 1/rate
+
+        #set the spike train
+        self.train = []
+        self.train.append(0)
+        self.train.append(timeDiff)
+
+
 
 class Neuron:
     def __init__(self):
@@ -39,13 +75,11 @@ class Neuron:
     def runEulers(self):
         #get current- run performWeightedSum
         current = self.performWeightedSum()
-        print("current is: "+ str(current))
 
         #Constants from the 1st assignment
         V_t = -0.040 #threshold voltage
         R_m = 10e6 #resistance constant
         C_m = 8e-3 #capacitance constant
-        TIME_DIFF = 0.001
         zeroed_time = 0
 
         while True:
@@ -68,10 +102,7 @@ class Neuron:
 
     def calculateAvgRate(self):
         if len(self.train) > 1:
-            print("calculating avg rate:")
     
-            print(self.train[0])
-            print(self.train[1])
 
             self.avgRate = 1/(self.train[1] - self.train[0])
         else: 
@@ -84,24 +115,32 @@ class Neuron:
 
             #perform oja's rule
             #constants:
-            L_R = 1e-3 #LEARNING_RATE
+            L_R = 9e-6 #LEARNING_RATE
 
             #get avg rate of curr neuron and avg rate of prev neuron
+            #TODO: switch v_i and v_j
             v_i = self.avgRate #Current (post-synaptic)
             v_j = synapse.pre.avgRate #Prev (pre-synaptic)
-
-            print("pre avg weight: "  + str(v_j))
-            print("post avg weight: " + str(v_i))
-            print("weight:" + str(synapse.weight))
-            synapse.weight += (L_R * ((v_i * v_j) - (synapse.weight * (v_i**2)))) * TIME_PER_NEURON
-            print("updated weight: " + str(synapse.weight))
+            
+            print("pre-update " + str(synapse.weight))
+            
+            #run eulers on the synapse weight
+            totalTime = 0
+            while True:
+                dw_dt =  (L_R * ((v_i * v_j) - (synapse.weight * (v_i**2)))) 
+                synapse.weight += dw_dt * TIME_DIFF
+                totalTime += TIME_DIFF
+                if totalTime >= TIME_PER_NEURON:
+                    break
+        
+            print("post-update " + str(synapse.weight))
 
     def updateNeuron(self):
         self.runEulers()
         self.calculateAvgRate()
         self.updateWeights()
         self.time += TIME_PER_NEURON
-        print("average rate: " + str(self.avgRate))
+        print("avgRate: " + str(self.avgRate))
 
 
     def performWeightedSum(self):
@@ -134,10 +173,7 @@ class Synapse:
     def __init__(self, pre, post):
         self.pre = pre
         self.post = post
-        self.weight = random()
-        neg  = random()
-        if neg < 0.5:
-            self.weight *= -1
+        self.weight = random() / math.sqrt(32) 
 
 
 def fullyConnect(arr1, arr2):
@@ -147,8 +183,14 @@ def fullyConnect(arr1, arr2):
             n1.output.append(newSynapse)
             n2.input.append(newSynapse)
 
+def makeSynapsesNegative(neur):
+    for syn in neur.output:
+        syn.weight *= -1
+
 
 def main():
+    global CASE1_RATE
+    global CASE3_RATE
 
     layer1 = []
     layer2 = []
@@ -160,7 +202,7 @@ def main():
     for i in range(2):
         layer1.append(Neuron())
 
-    #create 16 neurons in the 2nd layer
+    #create 8 neurons in the 2nd layer
     for i in range(8):
         layer2.append(Neuron())
 
@@ -172,6 +214,17 @@ def main():
     fullyConnect(layer1, layer2)
     fullyConnect(layer2, layer3)
 
+    makeSynapsesNegative(layer2[0]) 
+    makeSynapsesNegative(layer2[1])
+    print("LAYER 1")
+    for neuron in layer1:
+        for synapse in neuron.output:
+            print(synapse.weight)
+
+    print("LAYER 2")
+    for neuron in layer2:
+        for synapse in neuron.output:
+            print(synapse.weight)
     #attach "training neurons" to 1st and 3rd layers
 
     #LAYER1
@@ -195,13 +248,20 @@ def main():
     #LAYER3        
     #create training neurons
     trainLow = TrainingNeuron(None)
-    trainLow.updateCurrent(5e-4)
     trainHigh = TrainingNeuron(None)
-    trainHigh.updateCurrent(5e-4)
+
+    CASE1_RATE = currToRate(6.4e-5)
+    CASE3_RATE = currToRate(6.4e-5)
+
+    print("R1: " + str(CASE1_RATE))
+    print("R3: " + str(CASE3_RATE))
 
     #create training synapses
     synLow = Synapse(trainLow, layer3[0])
     synHigh = Synapse(trainHigh, layer3[1])
+
+    print("TESTING")
+    runNet(layer1, layer2, layer3, synLow, synHigh, trainA, trainB, trainLow, trainHigh, case=3, ctr=1)
 
     #attach synapses to postsynaptic neurons
     layer3[0].input.append(synLow)
@@ -209,16 +269,10 @@ def main():
     
     
     #neural net time with training neurons!
-    for i in range(TRAIN_LOOP):
-        runNet(layer1, layer2, layer3, synLow, synHigh, trainA, trainB, case=1, ctr=CTR)
-        runNet(layer1, layer2, layer3, synLow, synHigh, trainA, trainB, case=3, ctr=CTR)
-    """
-    for i in range(TRAIN_LOOP):
-        #runNet(layer1, layer2, layer3, synLow, synHigh, trainA, trainB, case=1, ctr=CTR)
-        runNet(layer1, layer2, layer3, synLow, synHigh, trainA, trainB, case=3, ctr=CTR)
-    """
-
-
+    for r in range(20):
+        runNet(layer1, layer2, layer3, synLow, synHigh, trainA, trainB, trainLow,trainHigh,case=3,ctr=1)
+        runNet(layer1, layer2, layer3, synLow, synHigh, trainA, trainB, trainLow,trainHigh,case=1,ctr=1)
+    
     #pop the training neurons and see if the net stil works (assuming last on list)
     for neuron in layer3:
         neuron.input.pop()
@@ -226,20 +280,27 @@ def main():
     print("trained results:")
     #neural net time without training neurons!
     print("case 1")
-    runNet(layer1, layer2, layer3, synLow, synHigh, trainA, trainB, case=1, ctr=1)
+    runNet(layer1, layer2, layer3, synLow, synHigh, trainA, trainB, trainLow, trainHigh, case=1, ctr=1)
     print("case 3")
-    runNet(layer1, layer2, layer3, synLow, synHigh, trainA, trainB, case=3, ctr=1)
+    runNet(layer1, layer2, layer3, synLow, synHigh, trainA, trainB, trainLow, trainHigh, case=3, ctr=1)
 
 
 
-def runNet(layer1, layer2, layer3, synLow, synHigh, trainA, trainB, case, ctr):
+def runNet(layer1, layer2, layer3, synLow, synHigh, trainA, trainB, trainLow, trainHigh, case, ctr):
    #TODO maybe my input weights aren't different enough? experiment
+
+    global CASE1_RATE
+    global CASE3_RATE
+    
     if case == 1:
         #A = 1, B = 1
         trainA.updateCurrent(HIGH_CURR)
         trainB.updateCurrent(HIGH_CURR)
         synLow.weight = ON_TRAIN
         synHigh.weight = OFF_TRAIN
+        trainHigh.updateCurrWithRate(CASE1_RATE)
+        trainLow.updateCurrWithRate(CASE1_RATE)
+        print("ASS: " + str(trainHigh.curr))
     elif case == 2:
         #A = 1, B = 0
         trainA.updateCurrent(HIGH_CURR)
@@ -252,6 +313,9 @@ def runNet(layer1, layer2, layer3, synLow, synHigh, trainA, trainB, case, ctr):
         trainB.updateCurrent(HIGH_CURR)
         synLow.weight = OFF_TRAIN
         synHigh.weight = ON_TRAIN
+        trainHigh.updateCurrWithRate(CASE3_RATE)
+        trainLow.updateCurrWithRate(CASE3_RATE)
+        print("HOLE: " + str(trainHigh.curr))
     else:
         #A = 0, B = 0
         trainA.updateCurrent(LOW_CURR)
@@ -260,23 +324,38 @@ def runNet(layer1, layer2, layer3, synLow, synHigh, trainA, trainB, case, ctr):
         synHigh.weight = OFF_TRAIN
 
     for i in range(ctr):
+        print("LAYER1")
         for n in layer1:
-            print("layer1 neuron")
+            #print("layer1 neuron")
             n.updateNeuron()
 
+        print("LAYER2")
         for n in layer2:
-            print("layer2 neuron")
+            #print("layer2 neuron")
             n.updateNeuron()
 
+        print("LAYER3")
         for n in layer3:
-            print("layer3 neuron")
+            #print("layer3 neuron")
             n.updateNeuron()
 
         deleteTrain(layer1)
         deleteTrain(layer2)
         deleteTrain(layer3)
+    
+        #update training weights
+        if case == 1:
+            CASE1_RATE = max(layer3[0].avgRate,layer3[1].avgRate) * 0.7
+            print("CASE1_RATE: " + str(CASE1_RATE))
+        elif case == 3:
+            CASE3_RATE = max(layer3[0].avgRate,layer3[1].avgRate) * 0.7
+            print("CASE3_RATE: " + str(CASE3_RATE))
+        print("TRAIN_WEIGHT: " + str(trainHigh.curr))
 
-        print("case " + str(case) + " low: " + str(layer3[0].avgRate) + " high: " + str(layer3[1].avgRate))
+    
+        #update the training current
+
+        #print("case " + str(case) + " low: " + str(layer3[0].avgRate) + " high: " + str(layer3[1].avgRate))
 
 def deleteTrain(layer):
     for neu in layer:
